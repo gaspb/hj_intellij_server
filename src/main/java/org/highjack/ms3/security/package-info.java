ac3979a3a8b6cc89fe37d3852300226e711e2082
gaspb <gaspard.boursin@gmail.com>
2018-08-18 21:17:45 +0200

    Initial application generated by JHipster-5.2.1

---
+++ src/main/java/org/highjack/ms3/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package org.highjack.ms3.security;
---
+++ src/main/java/org/highjack/ms3/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package org.highjack.ms3.web.rest.vm;
